2fd2cb2ad77ff45f0fe0567b712655a0853029fb
uzzzzzzzzzzzzzz <zga786338112@outlook.com>
2026-01-10 17:29:40 +0800

    fix build

--- src/Common/config.h
+++ src/Common/config.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#if defined(_WIN32) || defined(_WIN64) /* { */
 #if defined(SPT_BUILD_AS_DLL) /* { */
 #if defined(SPT_EXPORTS) /* { */
 #define SPT_API __declspec(dllexport)
@@ -8,8 +9,16 @@
 #endif /* } */
 #define SPT_API_CLASS SPT_API
 #else /* }{ */
-
 #define SPT_API extern
 #define SPT_API_CLASS
-
+#endif /* } */
+#else /* }{ */
+// 非 Windows 平台（Linux/macOS）
+#if defined(SPT_BUILD_AS_DLL) /* { */
+#define SPT_API __attribute__((visibility("default")))
+#define SPT_API_CLASS __attribute__((visibility("default")))
+#else /* }{ */
+#define SPT_API extern
+#define SPT_API_CLASS
+#endif /* } */
 #endif /* } */
